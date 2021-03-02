d9a2ded44db42904374c1f741ecb5b2e1a6c1fc3
yujin <claire9585@sookmyung.ac.kr>
2021-03-03 00:47:58 +0900

    subscriptions

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
+
 const countModifier = (count = 0, action) => {
   if (action.type === "ADD") {
     return count + 1;
@@ -16,6 +18,18 @@ const countModifier = (count = 0, action) => {
 
 const countStore = createStore(countModifier); // createStore은 우리에게 reducer를 주기를 요구함. reducer는 function이어야 함
 
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "MINUS" });
-console.log(countStore.getState());
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+};
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
